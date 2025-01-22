a281ebf5394ca5cb064fe8fbe1e9bc6930409ca5
Sumit-Ranjit <sumit.ranjit@oyorooms.com>
2025-01-22 14:17:03 +0530

    jk

--- delete.js
+++ delete.js
@@ -1,14 +1,30 @@
 window.onload = function() {
-    // Clear local storage
+    // Preserve login information
+    const loginInfo = localStorage.getItem('loginInfo');
+    const sessionLoginInfo = sessionStorage.getItem('loginInfo');
+    const loginCookie = document.cookie.split(";").find(c => c.trim().startsWith('loginInfo='));
+
+    // Clear local storage except login information
     localStorage.clear();
+    if (loginInfo) {
+        localStorage.setItem('loginInfo', loginInfo);
+    }
 
-    // Clear session storage
+    // Clear session storage except login information
     sessionStorage.clear();
+    if (sessionLoginInfo) {
+        sessionStorage.setItem('loginInfo', sessionLoginInfo);
+    }
 
-    // Clear cookies
-    document.cookie.split(";").forEach(function(c) { 
-        document.cookie = c.trim().split("=")[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/"; 
+    // Clear cookies except login information
+    document.cookie.split(";").forEach(function(c) {
+        if (!c.trim().startsWith('loginInfo=')) {
+            document.cookie = c.trim().split("=")[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+        }
     });
+    if (loginCookie) {
+        document.cookie = loginCookie;
+    }
 
-    console.log("All browser storage data has been deleted.");
+    console.log("All browser storage data except login information has been deleted.");
 };
